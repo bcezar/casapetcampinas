8f6b149bb93b8ddbcf735d58f49f1e2d692504a3
Bruno Dionisio Francisco <Bruno.DionisioFrancisco.Contractor@pepsico.com>
2024-06-22 11:54:16 -0300

    Page under construction

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './logo.png'; // Verifique se você tem um logo ou substitua o caminho da imagem.
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="Casa Pet Campinas logo" />
+        <h1>Casa Pet Campinas</h1>
+        <p>Site em Construção</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload. Agora Vai 2!
+          Agende um banho e tosa via WhatsApp:{" "}
+          <a
+            href="https://wa.me/5519995086273"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="App-link"
+          >
+            (19) 99508-6273
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Siga-nos no Instagram:{" "}
+          <a
+            href="https://www.instagram.com/casapet.campinas"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="App-link"
+          >
+            @casapet.campinas
+          </a>
+        </p>
+        <p>Entregamos via Ifood</p>
       </header>
     </div>
   );
